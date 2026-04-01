09f4dd96820c85500de733127c21d36ca24b775c
TroubleKavindu24 <it21834660@my.sliit.lk>
2026-04-01 14:31:55 +0530

    update asset routes

--- server/routes/assets.routes.js
+++ server/routes/assets.routes.js
@@ -2,13 +2,23 @@ const express = require("express");
 const router = express.Router();
 const assetController = require("../controllers/assets.controller");
 
+
+//Asset CRUD
 router.post("/add-asset", assetController.add);
 router.get("/assetsList", assetController.getAllAssets);
-router.post("/asset-allocation", assetController.allocateAsset);
 router.get("/asset-available/:type", assetController.getAvailableAssetsByType);
+
+// Allocation routes
+router.post("/asset-allocation", assetController.allocateAsset);
+router.get("/getAllAllocations", assetController.getAllAllocations);
+router.get("/allocations/branch/:branch_id", assetController.getAllocationsByBranch);
+
+// Handover routes
 router.post("/asset-handover", assetController.asset_handover);
+
+// Asset details
 router.get("/serial/:serial_no", assetController.getAssetDetailsBySerialNo);
 router.get("/serial/:serial_no/history", assetController.getAllocationHistoryBySerialNo);
-router.get("/getAllAllocations", assetController.getAllAllocations);
+
 
 module.exports = router;
