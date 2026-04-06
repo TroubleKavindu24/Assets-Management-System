bb7e91ce7eafe0d079ad017dbe2313f82fd48be8
TroubleKavindu24 <it21834660@my.sliit.lk>
2026-04-06 12:57:33 +0530

    asset model updated

--- server/models/Asset.js
+++ server/models/Asset.js
@@ -1,4 +1,4 @@
-// models/Asset.js (with ENUM for brand and OS)
+// models/Asset.js (with ENUM for brand and OS and new attributes)
 const { DataTypes } = require("sequelize");
 const { sequelize } = require("../config/db");
 
@@ -12,9 +12,12 @@ const Asset = sequelize.define("Asset", {
     type: DataTypes.STRING(100),
     allowNull: false,
     unique: true,
+    validate: {
+      notEmpty: true,
+    },
   },
   asset_type: {
-    type: DataTypes.ENUM("Laptop", "Machine", "Printer", "Other"),
+    type: DataTypes.ENUM("Laptop", "Desktop PC", "Monitor", "Printer", "Other"),
     allowNull: false,
   },
   brand: {
@@ -25,25 +28,47 @@ const Asset = sequelize.define("Asset", {
     type: DataTypes.ENUM("Windows 10", "Windows 11", "macOS", "N/A"),
     defaultValue: "N/A",
   },
+  purchase_date: {
+    type: DataTypes.DATE,
+  },
+  
   ram_capacity: {
     type: DataTypes.STRING(50),
     allowNull: true,
+    comment: "e.g., 8GB, 16GB, 32GB",
   },
   hard_drive: {
     type: DataTypes.STRING(100),
     allowNull: true,
+    comment: "e.g., 256GB SSD, 1TB HDD",
   },
   processor: {
     type: DataTypes.STRING(100),
     allowNull: true,
+    comment: "e.g., Intel i5, AMD Ryzen 7",
   },
-  warranty_period: {
+  
+  warranty_period_months: {
     type: DataTypes.INTEGER,
     allowNull: true,
+    validate: {
+      min: 0,
+      max: 120, // Maximum 10 years warranty
+    },
+    comment: "Warranty period in months",
   },
-  purchase_date: {
+  warranty_end_date: {
     type: DataTypes.DATE,
+    allowNull: true,
+    comment: "Automatically calculated based on purchase_date + warranty_period_months",
   },
+  
+  // Accessories
+  accessories: {
+    type: DataTypes.ENUM("Charger", "Bag", "Mouse"),
+    allowNull: true,
+  },
+  
   status: {
     type: DataTypes.ENUM("AVAILABLE", "ALLOCATED", "UNDER_REPAIR", "RETIRED"),
     defaultValue: "AVAILABLE",
@@ -52,6 +77,26 @@ const Asset = sequelize.define("Asset", {
   tableName: "assets",
   timestamps: true,
   underscored: true,
+  hooks: {
+    beforeCreate: async (asset) => {
+      if (asset.purchase_date && asset.warranty_period_months) {
+        const warrantyEndDate = new Date(asset.purchase_date);
+        warrantyEndDate.setMonth(warrantyEndDate.getMonth() + asset.warranty_period_months);
+        asset.warranty_end_date = warrantyEndDate;
+      }
+    },
+    beforeUpdate: async (asset) => {
+      if (asset.changed('purchase_date') || asset.changed('warranty_period_months')) {
+        if (asset.purchase_date && asset.warranty_period_months) {
+          const warrantyEndDate = new Date(asset.purchase_date);
+          warrantyEndDate.setMonth(warrantyEndDate.getMonth() + asset.warranty_period_months);
+          asset.warranty_end_date = warrantyEndDate;
+        } else {
+          asset.warranty_end_date = null;
+        }
+      }
+    },
+  },
 });
 
 module.exports = Asset;
